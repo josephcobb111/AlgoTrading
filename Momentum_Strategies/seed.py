fc894d54fa8fa7fdda636782bc4313d94172055a
josephcobb111 <joseph_cobb96@yahoo.com>
2020-12-07 21:52:41 -0500

    implement try/except logic; break algorithm if no day trades available

--- Momentum_Strategies/seed.py
+++ Momentum_Strategies/seed.py
@@ -4,12 +4,11 @@
 ## Contributors: Jeffrey Wubbenhorst
 
 ## Strategy
-## Find stocks within 99% of their 52 week high
+## Find stocks within 99.9% of their 52 week high
 ## Open long positions
-## Set trailing stop loss
+## Set take profit and stop loss levels
 
 ## TO DO:
-## Implement logic to avoid pattern day trader (PDT) label
 ## Implement logic to assess multiple tickers for trades
 ## Implement logic to limit open positions per ticker to 1
 ## Implement logic to limit position size
@@ -30,25 +29,41 @@ def robinhood_login(robin_user=os.environ.get('robinhood_username'), robin_pass=
              expiresIn=86400,
              by_sms=True)
 
-
 def get_next_market_open_hours(market='XNYS'):
     """Get next market open hours. Default market is NYSE for US market hours."""
     today_market_open_dict = rs.markets.get_market_today_hours(market=market)
-    if today_market_open_dict['is_open']:
+    current_time = parser.parse(datetime.now(timezone.utc).isoformat())
+    if today_market_open_dict['is_open'] and current_time < parser.parse(today_market_open_dict['opens_at']):
         market_opens = parser.parse(today_market_open_dict['opens_at'])
         market_closes = parser.parse(today_market_open_dict['closes_at'])
     else:
-
         next_market_open_dict = rs.markets.get_market_next_open_hours(market=market)
         market_opens = parser.parse(next_market_open_dict['opens_at'])
         market_closes = parser.parse(next_market_open_dict['closes_at'])
     return market_opens, market_closes
 
-
 def seconds_until_market_open(market_opens):
     """Get seconds until market opens."""
     current_time = parser.parse(datetime.now(timezone.utc).isoformat())
     return np.floor((market_opens - current_time).total_seconds())
+
+def check_for_day_trades():
+    """Get number of day trades used on the account"""
+    # check we have at least one day trade available
+    account = rs.profiles.load_account_profile('account_number')
+    url = rs.urls.daytrades(account)
+
+    headers = rs.globals.SESSION.headers
+    rs.globals.SESSION.headers['X-Robinhood-API-Version'] = '1.315.0'
+
+    results = rs.helper.request_get(url)
+    stock_day_trades = results['equity_day_trades']
+    options_day_trades = results['option_day_trades']
+    day_trades = len(stock_day_trades) + len(options_day_trades)
+
+    rs.globals.SESSION.headers = headers
+
+    return day_trades
 
 
 # create logger'
@@ -88,18 +103,35 @@ while True:
     # get next market open and close times
     try:
         market_opens, market_closes = get_next_market_open_hours()
+        logger.info('Market opens {} and closes {}.'.format(market_opens, market_closes))
     except:
         logger.info('Get market times failed.')
     current_time = parser.parse(datetime.now(timezone.utc).isoformat())
 
+    day_trades = check_for_day_trades()
+    if day_trades < 0 or day_trades > 2:
+        logger.info("WARNING: Must have less at least 1 day trades available to trade.")
+        break
     # while market is open, execute trading strategy
     while (current_time >= market_opens) & (current_time < market_closes):
         # get current portfolio and uninvested cash values
-        total_portfolio_value = float(rs.profiles.load_portfolio_profile()['equity'])
-        uninvested_cash = float(rs.account.load_phoenix_account()['uninvested_cash']['amount'])
+        try:
+            total_portfolio_value = float(rs.profiles.load_portfolio_profile()['equity'])
+        except:
+            logger.info('Failed to get total portfolio value.')
+        try:
+            uninvested_cash = float(rs.account.load_phoenix_account()['uninvested_cash']['amount'])
+        except:
+            logger.info('Failed to get uninvested cash.')
         # get latest price and 52 week high prices
-        latest_price = float(rs.stocks.get_latest_price(inputSymbols=ticker)[0])
-        high_52_weeks = float(rs.stocks.get_fundamentals(inputSymbols=ticker,info='high_52_weeks')[0])
+        try:
+            latest_price = float(rs.stocks.get_latest_price(inputSymbols=ticker)[0])
+        except:
+            logger.info('Failed to get latest ticker price.')
+        try:
+            high_52_weeks = float(rs.stocks.get_fundamentals(inputSymbols=ticker,info='high_52_weeks')[0])
+        except:
+            logger.info('Failed to get 52 week high.')
         # if <10% of portfolio is cash, do nothing
         if uninvested_cash/total_portfolio_value < cash_allocation:
             logger.info('Do nothing. Less than 10% of portfolio in cash.')
